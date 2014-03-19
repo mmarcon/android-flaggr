519cead241248722b7c3d6e51813242d01e3eecc
Massimiliano Marcon <me@marcon.me>
2014-03-19 07:56:57 +0100

    Robolectric + Gradle setup

--- flaggr/src/test/java/com/here/flaggr/intent/FlaggrIntentTest.java
+++ flaggr/src/test/java/com/here/flaggr/intent/FlaggrIntentTest.java
@@ -1,15 +1,21 @@
 package com.here.flaggr.intent;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
 
 /**
  * User: mmarcon
  * Date: 17/03/2014
  * Time: 17:10
  */
+@RunWith(RobolectricTestRunner.class)
 public class FlaggrIntentTest {
     @Test
     public void testParse() throws Exception {
-
+        assertEquals(true, false);
     }
 }
